48393cb615452f08ef550391f23d5184331db39d
Mubarig <mubarig_cisman@yahoo.com>
2020-08-02 20:55:48 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## This App can search a GitHub user  using the GitHub Public API I built it help of ReactJS Library.
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Form from './components/Form';
+import CardList from './components/CardList';
 
 function App() {
+    const [cards, setCards] = useState([]);
+    const addNewCard = card => setCards([...cards, card]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="App">
+          <header className="App-header">
+              <h1>SEARCH A GITHUB USER</h1>
+          </header>
+          <Form onSubmit={addNewCard} />
+          <CardList cards={cards} />
+      </div>
+  )
+};
 
-export default App;
+export default App
