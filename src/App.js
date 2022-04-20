85a54280ee02869c022fa3efb80f28abedbe827b
Mubarig Cisman <mubo1@gmail.com>
2022-04-20 11:47:07 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900'>
+        Search a GitHub User
+      </h1>
+    </div>
+  )
+  
+}
+
+export default App;
