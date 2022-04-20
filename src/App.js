fc8381eabc5de7b47371acc20b4ae51fd46b7e3e
Mubarig Cisman <mubo1@gmail.com>
2022-04-20 13:38:56 +0200

    GitHub API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import Form from "./components/Form";
 
 function App() {
+    const [cards, setCards] = useState([]);
+
+    const addNewCard = (card) => {
+        setCards( [...cards, card]);
+    }
+
     return (
-        <div class="justify-center items-center h-screen">
+        <div className="justify-center items-center h-screen">
             <h1 className='pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900'>
                 Search a GitHub User
             </h1>
-            <Form />
+            <Form onSubmit={addNewCard} />
         </div>
     )
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import { GoOctoface } from "react-icons/go";
+import axios from "axios";
 
 const Form = (props) => {
+    const [username, setUsername] = useState('');
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        axios.get(`https://api.github.com/users/${username}`).then((resp) =>{
+        props.onSubmit(resp.data)
+        });
+        setUsername('');
+
+    }     
 
     return (
-        <form className='w-full max-w-sm mx-auto'>
+        <form onSubmit={submitHandler} className="w-full max-w-sm mx-auto">
             <div className='mt-10 flex'>
+                <GoOctoface  size={50} className="mr-5"/>
                 <input
                     type='text'
-                    placeholder='GitHub username'
-                    className='form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150'
+                    placeholder='Search a GitHub username'
+                    className='form-input block w-full py-2 px-5 border border-blue-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150'
                     required
+                    value={username}
+                    onChange={(event)=>setUsername(event.target.value)}
                 />
                 <button
                     type='submit'
-                    className='ml-2 py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out'
+                    className='ml-2 py-2 px-3 border border-blue-300 rounded-md text-sm leading-3 font-large text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out'
                 >
                     Search
                 </button>
